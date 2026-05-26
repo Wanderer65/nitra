5b572d60231576c2fc4e587be1e3db52ea2f7b81
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-05-26 21:50:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
       <Clients />
       <SocialMedia />
       <ExportBanner />
-      /* <CTA /> */
+      {/* <CTA /> */}
       <Footer />
       <Floaters />
     </div>
